b54c96b64fe178acc0867ce2331b53a53ccec6b7
youjeong Jeong <wjddbwjd1205@naver.com>
2023-08-10 22:54:00 +0900

    Organize files

--- BEAKJOON/[2435] 기상청 인턴 신현수/src/Main.java
+++ BEAKJOON/[2435] 기상청 인턴 신현수/src/Main.java
@@ -7,8 +7,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(bf.readLine());
+		StringTokenizer st = new StringTokenizer(bf.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
